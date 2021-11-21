e652e5cceccbcef2495470a2cc7f163b24761f8d
nhanle2107 <lengocminhnhan@gmail.com>
2021-11-21 20:42:04 +0700

    service changes

--- server/services/database.js
+++ server/services/database.js
@@ -1,8 +1,4 @@
 const config = require("../config/db.config");
-var mysql = require("mysql");
+var mysql = require("mysql");                       // database
 var connection = mysql.createConnection(config);
 module.exports = connection;
-
-
-
-
